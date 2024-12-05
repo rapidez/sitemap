3a5e7e2f4b7c7751842d729203ae5a1be427ff39
Kevin Meijer <kevin@justbetter.nl>
2024-12-05 16:01:29 +0100

    Clear sitemap cache before job

--- src/Jobs/GenerateSitemapJob.php
+++ src/Jobs/GenerateSitemapJob.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Cache;
 use Rapidez\Sitemap\Models\Sitemap;
 
 class GenerateSitemapJob implements ShouldQueue
@@ -16,6 +17,10 @@ class GenerateSitemapJob implements ShouldQueue
 
     public function handle(): void
     {
+        $cacheKey = 'rapidez.sitemaps.'.$this->storeId;
+        if (Cache::has($cacheKey)) {
+            Cache::forget($cacheKey);
+        }
         Sitemap::getCachedByStoreId($this->storeId);
     }
 }
